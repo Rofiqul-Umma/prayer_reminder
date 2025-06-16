06729c22a29f3a494b2697cb3c045513f470f7d0
Rofiqul Umma <70629894+Rofiq126@users.noreply.github.com>
2025-06-16 09:45:31 +0700

    refactor: Replace log statements with debugPrint in DioLogger for improved logging consistency chore: Update easy_sidemenu version to 0.7.1 and provider dependency type in pubspec.yaml

--- lib/core/dio_logger.dart
+++ lib/core/dio_logger.dart
@@ -1,36 +1,36 @@
-import 'dart:developer';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 class DioLogger extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    log('*** Request ***');
-    log('URI: ${options.uri}');
-    log('Method: ${options.method}');
-    log('Headers: ${options.headers}');
-    log('Data: ${options.data}');
+    debugPrint('*** Request ***');
+    debugPrint('URI: ${options.uri}');
+    debugPrint('Method: ${options.method}');
+    debugPrint('Headers: ${options.headers}');
+    debugPrint('Data: ${options.data}');
     super.onRequest(options, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    log('*** Response ***');
-    log('URI: ${response.requestOptions.uri}');
-    log('Status Code: ${response.statusCode}');
-    log('Data: ${response.data}');
+    debugPrint('*** Response ***');
+    debugPrint('URI: ${response.requestOptions.uri}');
+    debugPrint('Status Code: ${response.statusCode}');
+    debugPrint('Data: ${response.data}');
     super.onResponse(response, handler);
   }
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
-    log('*** Error ***');
-    log('URI: ${err.requestOptions.uri}');
-    log('Message: ${err.message}');
+    debugPrint('*** Error ***');
+    debugPrint('URI: ${err.requestOptions.uri}');
+    debugPrint('Message: ${err.message}');
     if (err.response != null) {
-      log('Status Code: ${err.response?.statusCode}');
-      log('Data: ${err.response?.data}');
+      debugPrint('Status Code: ${err.response?.statusCode}');
+      debugPrint('Data: ${err.response?.data}');
     } else {
-      log('No response data');
+      debugPrint('No response data');
     }
     super.onError(err, handler);
   }
